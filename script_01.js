f7b7b72898d56b603c81638a08e0de1f3d792375
Lawrence <lawrence.langen@gmail.com>
2023-01-19 10:58:08 +0100

    Funktionalität mit Einzelparametern/1.Iteration

--- script_01.js
+++ script_01.js
@@ -7,6 +7,18 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
+output(getSentence("ich","bin","Max","Mütze"));
+function getSentence(word1,word2, word3,word4) {
+    const GAP = " ";
+    const PUNKT = ".";
+    let str = word1 + GAP + 
+              word2 + GAP + 
+              word3 + GAP + 
+              word4 + 
+              PUNKT;
+              
+    return str;
+}
 
 
 
