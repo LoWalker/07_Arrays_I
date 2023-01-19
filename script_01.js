c130a48743996177cc4af4a80aa7248aebf8fcc8
Lawrence <lawrence.langen@gmail.com>
2023-01-19 12:23:33 +0100

    Funktionalität_mit_Array1

--- script_01.js
+++ script_01.js
@@ -3,22 +3,27 @@
 /* Die Satzbau-Maschine | Arrays */
 
 /*** 02. Theorie: Array */
-let arr; 
-// arr = new Array(); //Konstruktor
-arr = []; // Literal
-arr = [2,7,11,4];
-arr = [true,false,true];
-arr = ["Ich","bin","Max","Mütze"];
+// let arr;
+// arr = new Array();  // Konstruktor
+// arr = []; // Literal
+// arr = [2,7,11,4];
+// arr = [true,false,true];
+// arr = ["Ich","bin","Max","Mütze"];
 
-output(arr);
-output(arr.length); //Anzahl der Elemente
-output(arr[0]); // Index 0 (1. Pos. im Array)
-output(arr[3]); // letzte Pos. / konkret
-output(arr[arr.length-1]); // letzte Pos. / allgemein
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[3]); // letze Pos. / konkret
+// output(arr[arr.length-1]); // letze Pos. / allgemein
 
-// () --> Parameter, Tests
+// () --> Parameter,Tests
 // {} --> Codeblöcke
 // [] --> Arrays, Index
+
+
+
+
+
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
@@ -37,6 +42,29 @@ function getSentence(word1,word2,word3,word4,word5, word6) {
 
     return str;
 }
+
+
+/*** 01a. Funktionalität mit Array 1  */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
+
+
 
 
 
